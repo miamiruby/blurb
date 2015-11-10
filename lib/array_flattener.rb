61d6d88eb26fd993dc8f862fb797f5c352b2207f
Paul Kruger <paul@miamiphp.org>
2015-11-10 15:31:05 -0500

    Adding basic spec

---
+++ lib/array_flattener.rb
@@ -0,0 +1,5 @@
+class Array
+    def flatten
+      return []
+    end
+end
---
+++ spec/array_flattener_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/array_flattener'
+
+RSpec.describe "Array" do
+  it "returns a flattened array of items" do
+    pending("add test for flattening array")
+  end
+end
