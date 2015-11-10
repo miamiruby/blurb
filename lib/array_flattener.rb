8a4fbe255a26ac771274bce7aa0ff36cb1813f3b
Paul Kruger <paul@miamiphp.org>
2015-11-10 17:29:51 -0500

    Fixing array flattner.rb

--- lib/array_flattener.rb
+++ lib/array_flattener.rb
@@ -1,6 +1,20 @@
 class Array
-    def flatten
-      self.flatten!
-      return self
+	def flatten
+		return self.flatten_recursively(self)
+	end
+
+    def flatten_recursively(set_of_numbers, n=[])
+        set_of_numbers.each do |a|
+         if a.is_a?(Array)
+            flatten_recursively(a,n)
+         else
+            n << a
+         end
+        end
+      n
+    end
+
+    def monkeypatch?
+    	return true
     end
 end
